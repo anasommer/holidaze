29e52b86f7877284c2cd5f69431c0b782d2c2c63
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-01-09 21:07:11 +0100

    installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Venue from './pages/venue/Venue';
 
 function App() {
-  return <p>Hello, I am an APP</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/venues' element={<List />} />
+        <Route path='/venues/:id' element={<Venue />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/venue/Venue.jsx
@@ -0,0 +1,5 @@
+const Venue = () => {
+  return <div>Venue Single Page</div>;
+};
+
+export default Venue;
