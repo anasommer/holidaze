e399efafcad18f8e711ccdbba6f536761492a470
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-01-12 15:35:58 +0100

    basic file architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Venue from './pages/venue/Venue';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <h1 className='text-3xl font-bold underline bg-rose-400'>Hello world!</h1>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/venues' element={<List />} />
         <Route path='/venues/:id' element={<Venue />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
