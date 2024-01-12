dd18bf6f928563ceb980b484dc0d4c1ae253c3a6
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-01-12 15:38:01 +0100

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import List from './pages/list/List';
-import Venue from './pages/venue/Venue';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline bg-rose-400'>Hello world!</h1>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/venues' element={<List />} />
-        <Route path='/venues/:id' element={<Venue />} />
+        <Route path='/' element={<HomePage />} />
+        {/* <Route path='/venues' element={<List />} />
+        <Route path='/venues/:id' element={<Venue />} /> */}
       </Routes>
     </>
   );
