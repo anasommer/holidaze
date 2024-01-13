04054b112f53c944a40fe577b09040264c837b01
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-01-13 16:26:55 +0100

    created Layout component and basic logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import Layout from './components/common/Layout';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
         {/* <Route path='/venues' element={<List />} />
         <Route path='/venues/:id' element={<Venue />} /> */}
       </Routes>
