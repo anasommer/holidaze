0c7334a1417565c868d6c1f68c372fddf92b120d
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-02-09 21:11:40 +0100

    added close on outside the modal funct

--- src/components/manager/DeleteVenue/DeleteVenueModal.jsx
+++ src/components/manager/DeleteVenue/DeleteVenueModal.jsx
@@ -30,19 +30,18 @@ const DeleteVenueModal = ({ isOpen, onClose, onDelete, venueId }) => {
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className='bg-gray-50 p-6 rounded-lg shadow-lg max-w-sm mx-auto'>
         {' '}
-        {/* Adjusted for Tailwind's color palette */}
         <h1 className='text-lg font-semibold mb-4 text-gray-900'>
           Do you want to delete this venue?
         </h1>
         <div className='flex flex-col space-y-3'>
           <button
-            onClick={onClose} // Close the modal without deleting
+            onClick={onClose}
             className='bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition-colors'
           >
             No
           </button>
           <button
-            onClick={handleDelete} // Proceed with deletion and close the modal
+            onClick={handleDelete}
             className='bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transition-colors'
           >
             Delete Venue
--- src/utils/modal.jsx
+++ src/utils/modal.jsx
@@ -1,18 +1,21 @@
 const Modal = ({ isOpen, onClose, children, showCloseButton = true }) => {
   if (!isOpen) return null;
 
+  const handleOutsideClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'>
-      <div className=' p-4 rounded-lg relative w-[60%]'>
-        {showCloseButton && (
-          <button
-            onClick={onClose}
-            className='absolute top-0 right-0 text-2xl font-semibold p-2 leading-none text-gray-600 mr-9 mt-4 hover:text-gray-900'
-            aria-label='Close'
-          >
-            &times;
-          </button>
-        )}
+    <div
+      className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'
+      onClick={handleOutsideClick}
+    >
+      <div
+        className='p-4 rounded-lg relative w-[60%]'
+        onClick={(e) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>
