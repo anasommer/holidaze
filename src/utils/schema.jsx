a4381e4c8a4437a38a46e12417b79522df51bf68
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-02-10 13:47:53 +0100

    refactored schema

--- src/utils/schema.jsx
+++ src/utils/schema.jsx
@@ -1,5 +1,8 @@
 import * as yup from 'yup';
 
+const defaultMedia =
+  'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?q=80&w=4170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
 const schema = yup.object({
   name: yup
     .string()
@@ -35,9 +38,7 @@ const createVenueSchema = yup.object().shape({
         ? originalValue
         : value
         ? [value]
-        : [
-            'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?q=80&w=4170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          ]
+        : [defaultMedia]
     )
     .optional(),
 
